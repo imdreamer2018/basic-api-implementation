b82f21cf1938a7114bc20c296a918f48c3b6167f
imdreamer <743295483@qq.com>
2020-09-16 13:31:15 +0800

    [yangqian] implement: add User class

--- src/main/java/com/thoughtworks/rslist/entity/RsEvent.java
+++ src/main/java/com/thoughtworks/rslist/entity/RsEvent.java
@@ -1,18 +1,17 @@
 package com.thoughtworks.rslist.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class RsEvent {
 
     private String eventName;
 
     private String keyWord;
 
-    public RsEvent(String eventName, String keyWord) {
-        this.eventName = eventName;
-        this.keyWord = keyWord;
-    }
+
 }
---
+++ src/main/java/com/thoughtworks/rslist/entity/User.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.rslist.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class User {
+
+    private String userName;
+
+    private Integer age;
+
+    private String gender;
+
+    private String email;
+
+    private Integer phone;
+
+}
